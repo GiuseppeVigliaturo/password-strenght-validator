7432ebc032fe8f415f9d4e2d2d40efe090f7455b
giuviglia94 <vigliaturo94@gmail.com>
2021-09-09 13:08:14 +0200

    controlli validazione

--- lib.js
+++ lib.js
@@ -1,3 +1,4 @@
+/**creando un oggetto posso accedere alle proprietà in qualsiasi funzione in questo modulo */
 const _v = {
     hasError : false,
     isValidPassword :false,
@@ -14,16 +15,14 @@ function formValidation(form, notifica) {
     checkPasswordStrenght();
 }
 
-function submitForm() {
-    
-}
 /**
  * 8 caratteri -> valida ma non sicura
  * 8 caratteri - 1 carattere speciale -> mediamente sicura
  * 10 caratteri - almeno 2 caratteri speciali -> molto sicura
  */
 
-function checkPasswordStrenght(params) {
+function checkPasswordStrenght() {
+    //_v.form.password accede all'input cin name="password"
     _v.form.password.addEventListener('keyup', (e)=>{
         console.log(e.target.value);
         const isValid = {
@@ -37,7 +36,7 @@ function checkPasswordStrenght(params) {
         /**controlli in base ai caratteri inseriti */
         if (pwd.length >= 8) {
             _v.passwordStrenght[0].classList.add('active');
-            if (regexCount(/[&%?!]/g, pwd === 1)) {
+            if (regexCount(/[&%?!]/g, pwd ) === 1) {
                 _v.passwordStrenght[1].classList.add('active')
             }
             isValid.isLow = true;
@@ -52,7 +51,81 @@ function checkPasswordStrenght(params) {
     });
 }
 
+function submitForm() {
+    _v.form.addEventListener('submit', (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        checkValidation();
+    }, true);
+}
+
+function checkValidation() {
+    try {
+        //controllo campi obbligatori
+        requiredFields();
+
+        //controllo validità email
+        isValidEmail();
+
+        //controllo validità password e corrispondenza con conferma
+        checkPassword();
+
+        //controlli superati
+        _v.notificationItem.textContent = 'La regisrtazione è avvenuta correttamente';
+    } catch (e) {
+        _v.notificationItem.textContent = e.message;
+    }
+}
+
 function regexCount(pattern, password) {
+    //password.match()ritorna un array con le corrispondenze trovate
+    //se non trova corrispondenze allora restituiamo un array vuoto altrimenti match torna null
     return (password.match(pattern) || []).length;
 }
+
+function requiredFields(){
+    let error;
+    _v.hasError = false;
+    _v.formItems.forEach(item => {
+
+        if (item !== 'checkbox' && item.required && item.value === '') {
+            error = true;
+        }
+
+        if (item === 'checkbox' && item.required && !item.checked) {
+            error = true;
+        }
+        if (error) {
+            _v.hasError = true;
+            item.classList.add('error');
+        }
+    });
+
+    if (_v.hasError) {
+        throw new Error('Compilare i campi obbligatori')
+    }
+}
+
+
+function isValidEmail() {
+    const pwd = _v.form.password.value;
+    const re_pwd = _v.form.re_password.value;
+
+
+    if (!_v.emailPattern.test(_v.form.email.value)) {
+        throw new Error('Email indicata non valida')
+    }
+    if (pwd !== re_pwd) {
+        throw new Error('Email indicata non valida')
+    }
+
+}
+
+function checkPassword() {
+    if (!_v.isValidPassword) {
+        throw new Error('Password indicata non valida')
+    }
+}
+
+
 export default formValidation;
